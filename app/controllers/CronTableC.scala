8ac3d5371fecffdc2dd543b97ad69bbe21ee0510
akyao <konama.akyao@gmail.com>
2015-10-07 22:20:21 +0900

    バリデーション、トランザクション

--- app/controllers/CronTableC.scala
+++ app/controllers/CronTableC.scala
@@ -4,6 +4,7 @@ import java.security.MessageDigest
 import javax.inject.Inject
 
 import form.CronForm
+import helpers.CronTimeHelper
 import models.{Cron, CronLine}
 import org.joda.time.DateTime
 import play.api.data.Forms._
@@ -31,59 +32,83 @@ class CronTableC @Inject()(val messagesApi: MessagesApi) extends Controller with
 
   val form = Form(
     mapping(
-      "cronText" -> text
+      "cronText" -> nonEmptyText
+        .verifying("16384文字以内で入力してください。", {_.length <= 16384 })
+        .verifying("1行あたり200文字まで入力してください", {_.split("\n").exists(n => n.length <= 200) })
     )(CronForm.apply)(CronForm.unapply)
   )
 
   def save = Action {implicit request =>
 
-    val cronForm = form.bindFromRequest.get
-    val cronText = cronForm.cronText
+    form.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest(views.html.cron_table.edit(formWithErrors))
+      },
+      formData => {
+        val cronText = formData.cronText
+        val cron = save(cronText)
+
+        val cronLines = CronLine.findAllBy(sqls.eq(CronLine.syntax("cl").cronId, cron.id))
+        Ok(views.html.cron_table.show(cron, cronLines))
+      }
+    )
+  }
+
+  def save(cronText: String): Cron = {
+
     val cronTextLines = cronText.split("\n")
-
-// TODO validation
-//    if len(cron_text) > 16384:
-//      raise Exception("too big text")
-//
-    if (cronTextLines.length > 1000){
-      //      raise Exception("too big text")
-    }
-
     val cronBody = Some(cronText)
     val now = Some(DateTime.now())
     val hashBytes = MessageDigest.getInstance("SHA-256").digest(now.toString.getBytes)
     val hash = Some(hashBytes.map("%02x".format(_)).mkString)
 
-    val cron = Cron.create(cronBody, hash, now, now)
+    DB localTx { implicit session =>
 
-    for ((lineRaw, i) <- cronTextLines.zipWithIndex){
-      // 連続した空白はタブを一つのスペースに置換
-      val lineText = lineRaw.trim.replaceAll("\\s{1,}", " ")
-      if (!lineText.isEmpty) {
-        val isComment = lineText.startsWith("#")
-        val isSetting = lineText.contains("=")
-        val isCronLine = !isComment && !isSetting
-        // TODO コメントも直後の行を説明するものとして保存しておきたい。(DSL)
-        if (isCronLine) {
-          val elements = lineText.split(" ")
-          CronLine.create(
-            cronId = cron.id,
-            line = Some(i),
-            body = Some(lineText),
-            command = Some(elements.slice(5, 1000).mkString(" ")),
-            month = Some(elements(3)),
-            day = Some(elements(2)),
-            week = Some(elements(4)),
-            hour = Some(elements(1)),
-            minute = Some(elements(0)),
-            createdAt = now,
-            updatedAt = now
-          )
+      val cron = Cron.create(cronBody, hash, now, now)
+
+      for ((lineRaw, i) <- cronTextLines.zipWithIndex){
+        // 連続した空白はタブを一つのスペースに置換
+        val lineText = lineRaw.trim.replaceAll("\\s{1,}", " ")
+        if (!lineText.isEmpty) {
+          val isComment = lineText.startsWith("#")
+          val isSetting = lineText.contains("=")
+          val isCronLine = !isComment && !isSetting
+          // TODO コメントも直後の行を説明するものとして保存しておきたい。(DSL)
+
+          if (isCronLine) {
+            val elements = lineText.split(" ")
+            if (elements.length < 6 ) {
+              throw new RuntimeException("行目()がcronとして解釈不可能です.")
+            }
+            // 解釈不可能な入力ははじく
+            val minute = elements(0)
+            val hour = elements(1)
+            if (CronTimeHelper.calcTimes(minute, 60)._2) {
+              throw new RuntimeException("分の設定が解釈不可能です")
+            }
+            if (CronTimeHelper.calcTimes(hour, 24)._2) {
+              throw new RuntimeException("時間の設定が解釈不可能です")
+            }
+            // TODO エラーどげな感じでハンドリングすっかな。
+
+            CronLine.create(
+              cronId = cron.id,
+              line = Some(i),
+              body = Some(lineText),
+              command = Some(elements.slice(5, 1000).mkString(" ")),
+              month = Some(elements(3)),
+              day = Some(elements(2)),
+              week = Some(elements(4)),
+              hour = Some(hour),
+              minute = Some(minute),
+              createdAt = now,
+              updatedAt = now
+            )
+          }
         }
       }
-    }
 
-    val cronLines = CronLine.findAllBy(sqls.eq(CronLine.syntax("cl").cronId, cron.id))
-    Ok(views.html.cron_table.show(cron, cronLines))
+      return cron
+    }
   }
 }
--- app/helpers/CronTimeHelper.scala
+++ app/helpers/CronTimeHelper.scala
@@ -1,42 +1,50 @@
 package helpers
 
+import scala.util.control.NonFatal
+
 object CronTimeHelper{
 
-  def calcTimes(time: String, to: Int): Seq[Int] ={
+  def calcTimes(time: String, to: Int): (Seq[Int], Boolean) ={
+
+    var hasError = false
 
     if (time == "*") {
-      return Range(0, 60)
+      return (Range(0, 60), false)
     }
 
     val h = time.replaceAll("\\s", "")
     var times = List[Int]()
 
-    for (he <- h.split(",")) {
+    try {
+      for (he <- h.split(",")) {
 
-      val dash = he.contains("-")
-      val slash = he.contains("/")
+        val dash = he.contains("-")
+        val slash = he.contains("/")
 
-      if (dash && !slash){
-        // 2-14
-        val hFromTo = he.split("-")
-        times = times ++ Range(hFromTo(0).toInt, hFromTo(1).toInt)
-      } else if(!dash && slash){
-        // TODO */n ?
-        val n = he.split("/")(1).toInt
-        times = times ++ Range(0, to, n)
-      } else if (dash && slash) {
-        // 2-15/5
-        val frontAndBack = he.split("/")
-        val (front, back) = (frontAndBack(0), frontAndBack(1).toInt)
-        val frontFromAndTo = front.split("-")
-        val (frontFrom, frontTo) = (frontFromAndTo(0).toInt, frontFromAndTo(1).toInt)
-        times = times ++ Range(frontFrom, frontTo, back)
-      } else {
-        // 2 数字だけ
-        times = times :+ he.toInt
+        if (dash && !slash) {
+          // 2-14
+          val hFromTo = he.split("-")
+          times = times ++ Range(hFromTo(0).toInt, hFromTo(1).toInt)
+        } else if (!dash && slash) {
+          // TODO */n ?
+          val n = he.split("/")(1).toInt
+          times = times ++ Range(0, to, n)
+        } else if (dash && slash) {
+          // 2-15/5
+          val frontAndBack = he.split("/")
+          val (front, back) = (frontAndBack(0), frontAndBack(1).toInt)
+          val frontFromAndTo = front.split("-")
+          val (frontFrom, frontTo) = (frontFromAndTo(0).toInt, frontFromAndTo(1).toInt)
+          times = times ++ Range(frontFrom, frontTo, back)
+        } else {
+          // 2 数字だけ
+          times = times :+ he.toInt
+        }
       }
+    }catch{
+      // 何もしない
+      case NonFatal(t) => hasError = true
     }
-
-    return times
+    return (times, hasError)
   }
 }
