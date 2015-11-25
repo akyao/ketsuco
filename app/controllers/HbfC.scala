70d29a27505c110f9041b9b2100f3ce5e65340c3
akyao <konama.akyao@gmail.com>
2015-11-25 17:48:08 +0900

    hatena API呼び出し部分の実装

--- app/controllers/HbfC.scala
+++ app/controllers/HbfC.scala
@@ -1,5 +1,10 @@
 package controllers
 
+import java.util.Formatter.DateTime
+
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.ExecutionContext.Implicits.global
+
 import java.security.MessageDigest
 import javax.inject.Inject
 
@@ -9,11 +14,17 @@ import models.{Cron, CronLine}
 import play.api.data.Forms._
 import play.api.data._
 import play.api.i18n._
+import play.api.libs.ws.{WSResponse, WSClient, WSRequest}
 import play.api.mvc._
 import scalikejdbc._
 
+import scala.concurrent.duration.Duration
+import scala.xml.{XML, Elem}
+import scala.concurrent.{Await, Future}
 
-class HbfC @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+// @Inject ()の意味
+class HbfC @Inject()(val messagesApi: MessagesApi) (ws: WSClient) extends Controller with I18nSupport {
 
   def index = Action {
     Ok(views.html.hbf.index())
@@ -23,11 +34,76 @@ class HbfC @Inject()(val messagesApi: MessagesApi) extends Controller with I18nS
     Ok(views.html.hbf.show())
   }
 
-  def create() = Action {
+  def create() = Action { request =>
     Ok(views.html.hbf.edit(form))
   }
 
   def save = Action { implicit request =>
+
+    // RSSを読み込み、記事のURL一覧を返す
+    def rssUrl2UrlList(rssUrl:String) :Seq[String] = {
+      // TODO 変数名がきたねえよ
+      val future = ws.url(rssUrl).get()
+      val unko = future.map { res =>
+        val gero:Seq[String] = res.xml \ "entry" \ "link" map { feed =>
+          feed.attribute("href").get.toString()
+        }
+        gero
+      }
+      return Await.result(unko, Duration.Inf)
+    }
+
+    // 記事の一覧を取得し、URLとそのブックマーク件数のリストを返す
+    def urlList2BookmarkCountList(urlList:Seq[String]) : Map[String, Int] = {
+      // http://developer.hatena.ne.jp/ja/documents/bookmark/apis/getcount
+      // TODO URLエスケープ
+
+      val paramList = urlList.map(url => "url=" + url)
+      val apiQueryString = paramList.mkString("&")
+      val apiUrl = "http://api.b.st-hatena.com/entry.counts?"
+
+      val future = ws.url(apiUrl + apiQueryString).get()
+      val unko = future.map { res =>
+        res.json.as[Map[String, Int]]
+      }
+
+      return Await.result(unko, Duration.Inf)
+    }
+
+    def url2BookmarkInfo(url:String) : Seq[String]= {
+      // http://developer.hatena.ne.jp/ja/documents/bookmark/apis/getinfo
+      val apiUrl = "http://b.hatena.ne.jp/entry/jsonlite/?url=" + url
+      val future = ws.url(apiUrl).get()
+      val unko = future.map { res =>
+        val chinko = res.json \ "bookmarks" \\ "user"
+        chinko.map(jsv => jsv.toString())
+      }
+
+      return Await.result(unko, Duration.Inf)
+    }
+
+    // TODO siteUrl -> baseUrl
+
+    // TODO baseUrl -> rssUrl
+    val rssUrl = "http://jkondo.hatenablog.com/feed"
+
+    // rssUrl -> urlList
+    val urlList = rssUrl2UrlList(rssUrl)
+
+    // urlList ->
+    val bookMarkList = urlList2BookmarkCountList(urlList)
+
+    for (tup <- bookMarkList) {
+// TODO     if (tup._2 > 0) {
+      if (tup._2 == 25) { // TODO test
+        val bookmarkInfo = url2BookmarkInfo(tup._1)
+        // TODO bookmark情報を保存する
+        println(bookmarkInfo)
+
+        // TODO APIコール回数、迷惑かけない頻度にすること
+      }
+    }
+
     Redirect(routes.HbfC.show("fff"))
   }
 
@@ -39,4 +115,5 @@ class HbfC @Inject()(val messagesApi: MessagesApi) extends Controller with I18nS
       })
     )(HbfForm.apply)(HbfForm.unapply)
   )
+
 }
